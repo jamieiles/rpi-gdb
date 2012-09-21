3fa0bec3f983e8831c3b56cdf1b00954b2f206f8
Jamie Iles <jamie@jamieiles.com>
2012-09-21 01:39:59 +0100

    uart: allow the UART interrupt to be configured.
    This is a really, really minimal IRQ configuration - just enable
    the IRQ in the hardware.  This assumes that someone has already
    installed a handler.  Dangerous!

--- uart.c
+++ uart.c
@@ -93,13 +93,17 @@ void uart_disable(void)
 }
 
 /* Enable for 115200,8n1.  Does anyone actually use anything else? */
-void uart_enable(void)
+void uart_enable(unsigned long flags)
 {
+	writel(UART0_BASE + UART_IMSC_REG_OFFS, 0);
 	writel(UART0_BASE + UART_ICR_REG_OFFS, 0x7FF);
 	writel(UART0_BASE + UART_IBRD_REG_OFFS, 1);
 	writel(UART0_BASE + UART_FBRD_REG_OFFS, 40);
-	writel(UART0_BASE + UART_LCRH_REG_OFFS,
-	       LCRH_FEN_MASK | LCRH_WLEN8_MASK);
+	writel(UART0_BASE + UART_LCRH_REG_OFFS, LCRH_WLEN8_MASK);
 	writel(UART0_BASE + UART_CR_REG_OFFS,
 	       CR_UART_EN_MASK | CR_TXE_MASK | CR_RXE_MASK);
+
+	if (flags & UART_INT_ENABLE)
+		writel(UART0_BASE + UART_IMSC_REG_OFFS,
+		       INT_RX);
 }
--- uart.h
+++ uart.h
@@ -5,6 +5,8 @@ void uart_putc(int c);
 int uart_getc(void);
 void uart_flush_rx(void);
 void uart_disable(void);
-void uart_enable(void);
+
+#define UART_INT_ENABLE	(1 << 0)
+void uart_enable(unsigned long flags);
 
 #endif /* __UART_H__ */
